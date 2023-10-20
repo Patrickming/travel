ba5c28e30fded9e0773459a7e9ab2ee8e8b53c7b
Patrickming <patrickmingx@gmail.com>
2023-10-20 23:44:41 +0800

    commit indexToinit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # travel
+
+文旅
+
+index -> 首页
+
+order -> 其它页面
---
+++ index/Myhtml/js/index.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+    $('#dm22').click(function () {
+        $(this).addClass("currents");
+    })
+})
